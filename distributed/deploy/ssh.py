7926b950d3234f760d3d1c320b5cecbf5ad6dfe5
Albert DeFusco <adefusco@continuum.io>
2016-09-18 07:47:33 -0600

    Added interface support
    interface= means that the host can be reached at
    '{HOSTNAME}-{INTERFACE}'
    
    For ip-over-ib this might mean something like n1001-ib. In this case
    interface='ib'.

--- distributed/deploy/ssh.py
+++ distributed/deploy/ssh.py
@@ -5,7 +5,6 @@ import socket
 import os
 import sys
 import traceback
-from collections import Counter
 
 try:
     from queue import Queue
@@ -294,11 +293,11 @@ class SSHCluster(object):
 
 
 class PBSCluster(SSHCluster):
-    def __init__(self, scheduler_port=8786, interface='eth0', nthreads = 0, nprocs = 1, logdir = None):
+    def __init__(self, scheduler_port=8786, interface=None, nthreads = 0, nprocs = 1, logdir = None):
         nodefile = os.environ.get('PBS_NODEFILE')
         if nodefile:
             with open(nodefile) as f:
-                nodes = Counter(l.strip() for l in f.readlines())
+                nodes = [l.strip() for l in f.readlines()]
         else:
             raise ValueError('Must be run within a PBS job')
 
@@ -306,10 +305,12 @@ class PBSCluster(SSHCluster):
             try:
                 nthreads = os.environ.get('PBS_NUM_PPN')
             except KeyError:
-                raise ValueError('Could not determine processes per node. Use the nthreads keyword argument')
+                raise ValueError('Could not determine threads per node. Use the nthreads keyword argument')
 
         #TODO: use a specific interface
+        if interface:
+            nodes = ['{:s}-{:s}'.format(n,interface) for n in nodes]
 
-        super(PBSCluster, self).__init__(scheduler_addr='127.0.0.1', scheduler_port=scheduler_port,
-                worker_addrs=nodes.keys(), nthreads=nthreads, nprocs=nprocs, logdir=logdir)
+        super(PBSCluster, self).__init__(scheduler_addr=nodes[0], scheduler_port=scheduler_port,
+                worker_addrs=set(nodes), nthreads=nthreads, nprocs=nprocs, logdir=logdir)
 
